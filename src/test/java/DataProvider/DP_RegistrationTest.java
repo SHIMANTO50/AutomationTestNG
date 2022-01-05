bf26e08150ebc77edb97f74ccd1db8a08640c278
SHIMANTO50 <shimantotsp@gmail.com>
2022-01-05 14:41:54 +0600

    Registration Complete

--- src/test/java/DataProvider/DP_RegistrationTest.java
+++ src/test/java/DataProvider/DP_RegistrationTest.java
@@ -18,7 +18,7 @@ public class DP_RegistrationTest {
     public static ExtentReports reports;
     public static ExtentTest test;
 
-    @BeforeSuite
+    @BeforeClass
     public static void chrome_launch(){
         System.setProperty("webdriver.chrome.driver","./src/main/resources/Driver/chromedriver.exe");
         driver=new ChromeDriver();
@@ -26,7 +26,7 @@ public class DP_RegistrationTest {
         driver.get("https://demo.opencart.com/index.php?route=account/register");
         System.out.println("Before Suite Executed.");
     }
-    @AfterSuite
+    @AfterClass
     public static void chrome_close(){
         driver.close();
         System.out.println("After Suite Executed.");
@@ -59,7 +59,7 @@ public class DP_RegistrationTest {
                                       String ConfirmPassword){
 
         //Implement Extent Report
-        htmlReporter =new ExtentHtmlReporter("./ExtentReports/DP_Registration.html");
+        htmlReporter =new ExtentHtmlReporter("./ExtentReports/DP_Registration4.html");
         reports=new ExtentReports();
         reports.attachReporter(htmlReporter);
 
@@ -75,33 +75,41 @@ public class DP_RegistrationTest {
         WebElement FirstName= driver.findElement(By.id("input-firstname"));
         FirstName.clear();
         FirstName.sendKeys(firstName);
+        test.log(Status.INFO, "FirstName Type"+firstName);
 
         WebElement LastName= driver.findElement(By.id("input-lastname"));
         LastName.clear();
         LastName.sendKeys(lastName);
+        test.log(Status.INFO, "LastName Type"+lastName);
 
         WebElement Email= driver.findElement(By.id("input-email"));
         Email.clear();
         Email.sendKeys(email);
+        test.log(Status.INFO, "Email Type"+email);
 
 
         WebElement telephone= driver.findElement(By.id("input-telephone"));
         telephone.clear();
         telephone.sendKeys(Telephone);
+        test.log(Status.INFO, "Telephone Type"+Telephone);
 
         WebElement password= driver.findElement(By.id("input-password"));
         password.clear();
         password.sendKeys(Password);
+        test.log(Status.INFO, "Password Type"+Password);
 
         WebElement PasswordConfirm= driver.findElement(By.id("input-confirm"));
         PasswordConfirm.clear();
         PasswordConfirm.sendKeys(ConfirmPassword);
+        test.log(Status.INFO, "ConfirmPassword Type"+ConfirmPassword);
 
         WebElement PrivacyPolicy= driver.findElement(By.name("agree"));
         PrivacyPolicy.click();
+        test.log(Status.INFO, "PrivacyPolicy check");
 
         WebElement Continue= driver.findElement(By.xpath("//*[@id=\"content\"]/form/div/div/input[2]"));
         Continue.click();
+        test.log(Status.INFO, "Continue Button Click");
 
 
 
@@ -115,20 +123,20 @@ public class DP_RegistrationTest {
         Object [][] data=new Object[2][6];
 
         //set 1
-        data [0][0]="Earth";
+        data [0][0]="Tuesday";
         data [0][1]="World";
-        data [0][2]="earth@test.com";
-        data [0][3]="123123";
-        data [0][4]="12345";
-        data [0][5]="123456";
+        data [0][2]="tuesday@test.com";
+        data [0][3]="123123212";
+        data [0][4]="12345678";
+        data [0][5]="12345678";
 
         //set 2
-        data [1][0]="Moon";
+        data [1][0]="Friday";
         data [1][1]="Planet";
-        data [1][2]="moon@test.com";
-        data [1][3]="123123";
-        data [1][4]="123421";
-        data [1][5]="123456";
+        data [1][2]="friday@test.com";
+        data [1][3]="1231235612";
+        data [1][4]="12342132";
+        data [1][5]="123421311";
 
         return data;
     }
